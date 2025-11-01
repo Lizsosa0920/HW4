20bd43d8021c67f94644e74103b1be28c6a0e38d
Lizbeth Sosa <lizsosa0920@gmail.com>
2025-10-31 20:51:44 -0600

    Update hw4.py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,1 +1,8 @@
+# HW4 Stock Market Trading
+# this program will read the TESLA stock prices
+# it will print each trade's buy and sell price, profit, total profit,
+# first buy price, and overall percent return
+# -------
+
+# First, read tesla prices from TSLA.txt
 
